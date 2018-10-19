90fa9c6bff95b1a7a74e073e5341c9cd2d34d53a
Chris Laprun <metacosm@gmail.com>
2018-10-19 13:48:54 +0200

    Format documentation according to conventions.

--- pkg/occlient/occlient.go
+++ pkg/occlient/occlient.go
@@ -2121,9 +2121,10 @@ type Service struct {
 	PlanList []string
 }
 
-func (c *Client) GetServiceClass(className string) (class *scv1beta1.ClusterServiceClass, err error) {
+// GetServiceClass retrieves the ClusterServiceClass identified by the specified external name
+func (c *Client) GetServiceClass(externalName string) (class *scv1beta1.ClusterServiceClass, err error) {
 	classes, err := c.serviceCatalogClient.ClusterServiceClasses().List(metav1.ListOptions{
-		FieldSelector: "spec.externalName==" + className,
+		FieldSelector: "spec.externalName==" + externalName,
 	})
 
 	if len(classes.Items) != 1 {
@@ -2133,6 +2134,7 @@ func (c *Client) GetServiceClass(className string) (class *scv1beta1.ClusterServ
 	return &classes.Items[0], err
 }
 
+// GetServiceClassesByCategory retrieves a map associating category name to ClusterServiceClasses matching the category
 func (c *Client) GetServiceClassesByCategory() (categories map[string][]scv1beta1.ClusterServiceClass, err error) {
 	categories = make(map[string][]scv1beta1.ClusterServiceClass)
 	classes, err := c.GetClusterServiceClasses()
@@ -2154,6 +2156,8 @@ func (c *Client) GetServiceClassesByCategory() (categories map[string][]scv1beta
 	return categories, err
 }
 
+// GetMatchingPlans retrieves a map associated service plan name to service plan instance associated with the specified service
+// class
 func (c *Client) GetMatchingPlans(class scv1beta1.ClusterServiceClass) (plans map[string]scv1beta1.ClusterServicePlan, err error) {
 	planList, err := c.serviceCatalogClient.ClusterServicePlans().List(metav1.ListOptions{
 		FieldSelector: "spec.clusterServiceClassRef.name==" + class.Spec.ExternalID,
